960ded4c1380c450446df17cc9e5c602590b1221
dimi34alex <dimi_alex@mail.ru>
2023-03-27 11:52:00 +0300

    s

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -5,17 +5,22 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
     public Transform[] points;
-    public float speed;
-    public float chasingRange;
-    public float shootingDelay;
+    public float speed = 1f;
+    public float chasingRange = 10f;
+    public float chasingTime = 2f;
+    public float shootingDelay = 2f;
     public Transform shootingPoint;
     public AudioClip shootSound;
     public Animator animator;
     public GameObject laserPrefab;
-    public float timerBreaking;
-
+    public float timerBreaking = 2f;
+    public GameObject player;
+    public LayerMask mask;
+    public Rigidbody2D vision;
 
     private float timer;
+    private GameObject laser;
+    private bool isPatroling = true;
     private bool breaking = false;
     private Vector3 currentTarget;
     private int currentTargetIndex = 0; // индекс текущей цели
@@ -27,7 +32,7 @@ public class EnemyController : MonoBehaviour
     {
         if (breaking)
         {
-            animator.SetBool("IsBreaking", true);
+            //animator.SetBool("IsBreaking", true);
             if (timer <= timerBreaking)
             {
                 timer += Time.deltaTime;
@@ -36,24 +41,59 @@ public class EnemyController : MonoBehaviour
         else
         {
             timer = 0;
-            animator.SetBool("IsBreaking", false);
-            Patrol();
+            //animator.SetBool("IsBreaking", false);
+            if (isPatroling)
+                Patrol();
+            
         }
     }
-    private void Chase()
+    private void OnTriggerStay2D(Collider2D collision)
     {
-
+        Debug.Log("Ты вошел в меня");
+        if (collision.name == "Hero")
+        {
+            Debug.Log("Проверяю игрок ли это");
+            isPatroling = false;
+            if (timer <= chasingTime)
+                timer += Time.deltaTime;
+            else
+                Shoot();
+        }
+        else
+            Debug.Log("Не нашел(");
     }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("Потерял");
+        isPatroling = true;
+        timer = 0;
+    }
+
     private void Shoot()
     {
-
+        timer = 0;
+        Debug.Log("Стреляю");
+        laser = Instantiate(laserPrefab, shootingPoint);
+        Rigidbody2D rbLaser = laser.GetComponent<Rigidbody2D>();
+        rbLaser.AddForce(new Vector2(10, 0));
+    }
+    private void OnBecameInvisible()
+    {
+        Destroy(laser);
     }
     private void Patrol()
-    {
+    { 
+        //Debug.Log("Вошел в патруль");
         transform.position = Vector3.Lerp(transform.position, currentTarget, speed * Time.deltaTime);
+        //Debug.Log("Перемещаю");
         if (Vector3.Distance(transform.position, currentTarget) < 0.1f)
         {
-            currentTargetIndex = (currentTargetIndex + 1) % points.Length;
+            if (currentTargetIndex + 1 == points.Length)
+            {
+                currentTargetIndex = 0;
+            }
+            else
+                currentTargetIndex += 1;
             currentTarget = points[currentTargetIndex].position;
         }
     }
